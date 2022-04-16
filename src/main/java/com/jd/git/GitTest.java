9a63148708976fd5f6e2f44f9d133f6f6507426b
Noah <131119@qq.com>
2022-04-16 17:32:01 +0800

    first commit

---
+++ src/main/java/com/jd/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jd.git;
+
+/**
+ * @author Noah
+ * @date 2022-04-16 - 17:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
